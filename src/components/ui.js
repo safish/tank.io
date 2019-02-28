1171a3dae4a509a373ba0db7f6d1b43f6fba0cd1
JetLu <lufei@tuia.cn>
2019-02-28 21:18:33 +0800

    update

--- src/components/ui.js
+++ src/components/ui.js
@@ -5,3 +5,37 @@ document.querySelector('.btn.join').addEventListener('click', ev => {
   // connect()
   console.log(name)
 })
+
+{
+  const
+    container = document.createElement('div'),
+    dialog = document.querySelector('.dialog'),
+    btn = document.querySelector('.btn.join')
+
+  container.classList.add('d-flex', 'skin')
+  dialog.insertBefore(container, btn)
+
+  +['blue', 'green', 'dark', 'sand'].forEach(item => {
+    const
+      child = document.createElement('div'),
+      img = document.createElement('img')
+    img.src = `/static/images/tank.${item}.png`
+    child.appendChild(img)
+    container.appendChild(child)
+  })
+
+  container.addEventListener('click', ev => {
+    let target
+    ev.target.parentElement.classList.contains('skin') ?
+      target = ev.target : ev.target.parentElement.parentElement.classList.contains('skin') ?
+      target = ev.target.parentElement : 0
+
+    if (!target) return
+
+    Array.from(container.children).forEach(item => item.classList.remove('active'))
+
+    target.classList.add('active')
+  })
+}
+
+export default null
